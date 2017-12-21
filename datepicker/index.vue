8c2b423a473fe6537adbf3133023160d9b035ec4
Aaron Rubin <aaron@arkitech.net>
2017-12-21 15:39:39 -0500

    Update formatter to use moment

--- datepicker/index.vue
+++ datepicker/index.vue
@@ -179,28 +179,10 @@ export default {
       }
     },
     formatDate (date, fmt) {
-      const map = {
-        'M+': date.getMonth() + 1, // 月份
-        '[Dd]+': date.getDate(), // 日
-        '[Hh]+': date.getHours(), // 小时
-        'm+': date.getMinutes(), // 分
-        's+': date.getSeconds(), // 秒
-        'q+': Math.floor((date.getMonth() + 3) / 3), // 季度
-        'S': date.getMilliseconds() // 毫秒
-      }
-      let str = fmt.replace(/[Yy]+/g, function (str) {
-        return ('' + date.getFullYear()).slice(4 - str.length)
-      })
-      Object.keys(map).forEach((key) => {
-        str = str.replace(new RegExp(key), function (str) {
-          const value = '' + map[key]
-          return str.length === 1 ? value : ('00' + value).slice(value.length)
-        })
-      })
-      return str
+      return moment(date).format(fmt)
     },
     stringify (date) {
-      return this.formatDate(new Date(date), this.format)
+      return this.formatDate(date, this.format)
     },
     isValidDate (date) {
       return !!new Date(date).getTime()
